2d6d17ee2bfc5ed602584711799cef2960b81f78
Zoë <zoe.vandijk@casema.nl>
2023-03-25 15:04:29 +0100

    Finished week 5

--- Week_5/Opdracht_3/Opdracht_3.ino
+++ Week_5/Opdracht_3/Opdracht_3.ino
@@ -5,6 +5,9 @@ int POT_read;
 int POT_percentage;
 int LED_out_notCor;
 int LED_out_cor; 
+int LED_OUT_percentage;
+int LED_OUT_corrected_percentage;
+const float correctie = 0.714;
 
 void setup(){
   Serial.begin(9600);
@@ -21,22 +24,34 @@ void setup(){
 void loop(){
 // read and calculate
   POT_read = analogRead(POTPIN);
-  LED_out_notCor = map(POT_read, 0, 1023, 0, 255);
   POT_percentage = (POT_read / 1023.0) * 100;
-  LED_out_cor = (255 / 100.0) * POT_percentage;
+  
+  LED_out_notCor = (255 / 100.0) * POT_percentage;
+  LED_out_cor = LED_out_notCor * correctie;
 
+  LED_OUT_percentage = (LED_out_notCor / 255.0) * 100;
+  LED_OUT_corrected_percentage =  (LED_out_cor / 255.0)* 100;
+  
 // Print values
+  Serial.println("\n\n");
   Serial.print("Readed value from potmeter: ");
   Serial.println(POT_read);
   
   Serial.print("Percentage of potmeter: ");
   Serial.println(POT_percentage);
   
+  Serial.println("\n");
+//  No correction  
   Serial.print("LED brightness without correction: ");
   Serial.println(LED_out_notCor);
-
+  Serial.print("LED brightness percentage without correction: ");
+  Serial.println(LED_OUT_percentage);
+  Serial.println("\n");
+// Corrected 
   Serial.print("LED brightness with correction: ");
   Serial.println(LED_out_cor);
+  Serial.print("LED brightness percentage with correction: ");
+  Serial.println(LED_OUT_corrected_percentage);
 
 // Set leds on 
   analogWrite(LED_notCor, LED_out_notCor);

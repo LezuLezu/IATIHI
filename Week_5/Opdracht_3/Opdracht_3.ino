ab7e14a5d2fe331495f215fc66c1163779674d2b
Zoë <zoe.vandijk@casema.nl>
2023-03-25 12:37:11 +0100

    Week 5 complete, review with teacher

--- Week_5/Opdracht_3/Opdracht_3.ino
+++ Week_5/Opdracht_3/Opdracht_3.ino
@@ -1,25 +1,47 @@
 #define POTPIN A0
-const int LED = 5;
-const int LED_2 = 6;
+const int LED_cor = 3;
+const int LED_notCor = 5;
 int POT_read;
-int LED_out; 
+int POT_percentage;
+int LED_out_notCor;
+int LED_out_cor; 
 
 void setup(){
   Serial.begin(9600);
+  
   pinMode(POTPIN,INPUT);
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED, LOW);
-  pinMode(LED_2,OUTPUT);
-  digitalWrite(LED_2, LOW);
+  
+  pinMode(LED_notCor,OUTPUT);
+  digitalWrite(LED_notCor, LOW);
+  
+  pinMode(LED_cor,OUTPUT);
+  digitalWrite(LED_cor, LOW);
 }
 
 void loop(){
+// read and calculate
   POT_read = analogRead(POTPIN);
-  LED_out = map(POT_read, 0, 1023, 0, 255);
-  analogWrite(LED, LED_out);
-  Serial.print("Pot reading: ");
+  LED_out_notCor = map(POT_read, 0, 1023, 0, 255);
+  POT_percentage = (POT_read / 1023.0) * 100;
+  LED_out_cor = (255 / 100.0) * POT_percentage;
+
+// Print values
+  Serial.print("Readed value from potmeter: ");
   Serial.println(POT_read);
-  Serial.print("Calculated out: ");
-  Serial.println(LED_out);  
+  
+  Serial.print("Percentage of potmeter: ");
+  Serial.println(POT_percentage);
+  
+  Serial.print("LED brightness without correction: ");
+  Serial.println(LED_out_notCor);
+
+  Serial.print("LED brightness with correction: ");
+  Serial.println(LED_out_cor);
+
+// Set leds on 
+  analogWrite(LED_notCor, LED_out_notCor);
+  analogWrite(LED_cor, LED_out_cor);
+
   delay(50);
+  
 }
